b7b797c071d836fc0300730dee9276dfc4adba79
BubblyFace <751835145@qq.com>
2020-12-25 20:52:31 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Component from './component';
+import './index.less    '
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<Component />, mountNode);
